f86f65d5574a1fc63960943b73535218e44a6d32
Joao Viana <j.bernardo.viana@hotmail.com>
2021-08-30 17:05:37 +0100

    new version with CD

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ quote  = get_quote()  # assuming the function returns an author and a quote
 #f"{quote['quote']}, {quote['show']}, {quote['role']}"
 st.write(quote)
 
-
 '# Title'
 '## Sub title'
 '### sub sub title'
+1234567
