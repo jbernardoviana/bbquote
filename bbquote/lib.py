b9d4656cfe42c4d5697c3892c189f03a2797f33f
Joao Viana <j.bernardo.viana@hotmail.com>
2021-08-30 16:32:36 +0100

    init

---
+++ bbquote/lib.py
@@ -0,0 +1,12 @@
+# bbquote/lib.py
+import requests
+
+def get_quote():
+    url = 'https://movie-quote-api.herokuapp.com/v1/quote/'  # alternative API
+    response = requests.get(url).json()
+
+    return f"'{response['quote']}' \n> {response['role']}, {response['show']}"
+
+
+if __name__ == "__main__":
+    print(get_quote())
